1aa1aa71d5c6e1c3636576e30ea103827c39c63f
XTard <ickomcdevill@gmail.com>
2016-11-26 16:22:47 +0200

    Update refreshPoints.php
    Replace the table i've been using with table which you need to specify.

--- refreshPoints.php
+++ refreshPoints.php
@@ -1,14 +1,14 @@
 <?php
 include "mysql.php";
-$query = "select sp, xp from points where id='1'";
+$query = "select sp, xp from ". $table ." where id='1'";
 $result = mysqli_query($mysql, $query);
 $row = mysqli_fetch_assoc($result);
 echo "
     <div class='body-points-div'>
-    <img class='body-points-img' src='/ClickClickSpam/images/sp.png'>" . $row["sp"] . "
+      <img class='body-points-img' src='/ClickClickSpam/images/sp.png'>" . $row["sp"] . "
     </div>
     <div class='body-points-div-two'>
-    <img class='body-points-img' src='/ClickClickSpam/images/xp.png'>" . $row["xp"] . "
+      <img class='body-points-img' src='/ClickClickSpam/images/xp.png'>" . $row["xp"] . "
     </div>
 ";
 ?>
