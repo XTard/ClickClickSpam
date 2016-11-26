26cce44bc52ee35a28fbfca18caaed1e3c961676
XTard <ickomcdevill@gmail.com>
2016-11-26 13:56:00 +0200

    Update points.php
    Removed some useless code.

--- points.php
+++ points.php
@@ -1,12 +1,9 @@
 <?php
-include "mysql.php";
-$query = "select sp, xp from points where id='1'";
+require_once "mysql.php";
+$query = "select sp, xp from " . $table . " where id='1'";
 $result = mysqli_query($mysql, $query);
 $row = mysqli_fetch_assoc($result);
 $new_points = $row["sp"] + 1;
-$query2 = "update points set sp = '" . $new_points . "' where id = '1'";
-$asd = mysqli_query($mysql, $query2);
-$query = "select sp, xp from points where id='1'";
+$query = "update ". $table ." set sp = '" . $new_points . "' where id = '1'";
 $result = mysqli_query($mysql, $query);
-$row = mysqli_fetch_assoc($result);
 ?>
