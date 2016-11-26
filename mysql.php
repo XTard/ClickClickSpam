8a5aebcb14b66f67a9a4d7f9942dd7b4cf8ac439
XTard <ickomcdevill@gmail.com>
2016-11-26 13:37:00 +0200

    Create mysql.php which was missing

---
+++ mysql.php
@@ -0,0 +1,7 @@
+<?php
+$user = ""; //Your mysql username
+$password = ""; //Your mysql password
+$db = ""; //The database you are going to use
+$table = ""; //The table you are going to use
+$mysql = mysqli_connect($user, $password, $db);
+?>
